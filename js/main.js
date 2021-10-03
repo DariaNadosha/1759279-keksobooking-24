8df7147626867c706ed944f74b910094b81f4517
Daria Nadosha <daria.nadosha@gmail.com>
2021-10-03 19:37:14 +0300

    созданы вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+function getRandomInt (min,max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  if (max <= min || min<0) {
+    return('Функциия не может быть выполнена')
+  }
+
+  return Math.floor(Math.random() * (max-min)) + min;
+}
+console.log(getRandomInt(0,100));
+//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+function getRandom(min, max, fixed=0) {
+  if (max <= min || min<0) {
+    return('Функциия не может быть выполнена')
+  }
+  return Math.random().toFixed(fixed) * (max - min) + min;
+}
+console.log(getRandom(0,100,2));
