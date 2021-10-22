ff1542442c02c515d3050620b5f8d727852c1aa0
Daria Nadosha <daria.nadosha@gmail.com>
2021-10-22 20:24:34 +0300

    Исправлены замечания

--- js/main.js
+++ js/main.js
@@ -15,7 +15,33 @@ function getRandom(min, max, fixed=0) {
 getRandom(0,100,2);
 // console.log(getRandom(0,100,2));
 
-const AMOUNT_OF_OBJECTS = 10;
+function getPrice (min,max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return (max <= min || min<0) ? 'Функция не может быть выполнена'
+  :Math.floor(((Math.random() * (max-min)) + min)/500)*500;
+}
+const getRandomElement = (array) => {
+  return array[Math.floor(Math.random()*array.length)];
+};
+const createLocation = () => ({
+  lat: getRandom (35.65, 35.7),
+  lng: getRandom (139.7, 139.8),
+});
+const createArray  = (array) => {
+  const maxLength = array.length;
+  const lengthOfArray = getRandomInt(1, maxLength);
+  const newArray = [];
+  while (newArray.length < lengthOfArray) {
+    const indexOfEl = getRandomInt(0, maxLength-1);
+    const el = array[indexOfEl];
+
+    if (!newArray.includes(el)) {
+      newArray.push(el);
+    }
+  }
+  return newArray;
+}
 
 const AVATAR = [
   'img/avatars/user01.png',
@@ -31,14 +57,6 @@ const AVATAR = [
 ];
 const TITLE = 'Идеальное предложение!';
 const ADDRESS = '{{location.lat}}, {{location.lng}}';
-function getPrice (min,max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return (max <= min || min<0) ? 'Функциия не может быть выполнена'
-   :Math.round(((Math.random() * (max-min)) + min)/500)*500;
-  }
-  getPrice(1000,50000);
-//console.log (getPrice(1000,50000));
 const TYPE = [
   'palace',
   'flat',
@@ -46,28 +64,7 @@ const TYPE = [
   'bungalow',
   'hotel',
 ];
-function getRooms (min,max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return (max <= min || min<0) ? 'Функциия не может быть выполнена'
-   :Math.floor(Math.random() * (max-min)) + min;
-  }
-  getRooms(1,5);
-  //console.log (getRooms(1,5));
-function getGuests (min,max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return (max <= min || min<0) ? 'Функциия не может быть выполнена'
-    :Math.floor(Math.random() * (max-min)) + min;
-  }
-  getGuests(1,11);
-  //console.log (getGuests(1,11));
-const CHECKIN = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-const CHECKOUT = [
+const CHECKIN_CHECKOUT = [
   '12:00',
   '13:00',
   '14:00',
@@ -80,46 +77,41 @@ const FEATURES = [
   'elevator',
   'conditioner',
 ];
-const DESCRIPTION = 'Текст не добавлен. Добавить';
+const DESCRIPTION = 'Здесь вы сможете провести отпуск ни о чем не беспокоясь.';
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-function getLat(min, max, fixed=0) {
-  return (max <= min || min<0) ? 'Функциия не может быть выполнена'
-  :Math.random().toFixed(fixed) * (max - min) + min;
-}
-getLat(35.65000,35.70000,8);
-// console.log(getLat(35.65000,35.70000,8));
+const createSingleOffer = () => {
+  const location = createLocation();
 
-function getLng(min, max, fixed=0) {
-  return (max <= min || min<0) ? 'Функциия не может быть выполнена'
-  :Math.random().toFixed(fixed) * (max - min) + min;
-}
-getLng(139.70000,139.80000,2);
-// console.log(getLng(0,100,8));
-
-const infOfOffer = () => {
   return{
-    author: 'Автор объявления: ' + AVATAR[Math.floor(Math.random()*AVATAR.length)],
-    offer: 'Информация об объявлении: ' + TITLE + ';' +
-    'Адрес: ' + ADDRESS + ';' +
-    'Цена: ' + getPrice(1000,50000) + ';' +
-    'Вид объекта: ' + TYPE[Math.floor(Math.random()*TYPE.length)] + ';' +
-    'Количество комнат: ' + getRooms(1,5) + ';' +
-    'Количество гостей: ' + getGuests(1,11) + ';' +
-    'Время заселения: ' + CHECKIN[Math.floor(Math.random()*CHECKIN.length)] + ';' +
-    'Дополнительно: ' + FEATURES[Math.floor(Math.random()*FEATURES.length)] + ';' +
-    'Описание: ' + DESCRIPTION + ';' +
-    'Фотографии: ' + PHOTOS[Math.floor(Math.random()*PHOTOS.length)],
-    location: 'Расположение: ' + getLat(35.65000,35.70000,8) + ';' + getLng(139.70000,139.80000,2),
+    author: {
+      avatar: getRandomElement(AVATAR),
+    },
+    offer: {
+      title: TITLE,
+      address: `${location.lat} ${location.lng}`,
+      price: getPrice(1000,50000),
+      type: getRandomElement (TYPE),
+      rooms: getRandomInt(1,5),
+      guests: getRandomInt(1,8),
+      checkin: getRandomElement (CHECKIN_CHECKOUT),
+      checkout: getRandomElement (CHECKIN_CHECKOUT),
+      features: createArray (FEATURES),
+      description: DESCRIPTION,
+      photos: createArray (PHOTOS),
+    },
+    location: location,
   }
 }
-//console.log(infOfOffer());
+//console.log(createSingleOffer());
+
+const AMOUNT_OF_OBJECTS = 10;
 const varietiesOfBooking = Array.from(
-  {length: AMOUNT_OF_OBJECTS}, infOfOffer
+  {length: AMOUNT_OF_OBJECTS}, createSingleOffer
   );
 
 //console.log(varietiesOfBooking);
